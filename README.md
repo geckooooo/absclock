b231109a64ab1e0d93c26c03c34a375259811100
Jason Wells <jason@wells.me>
2025-07-23 09:52:39 -0700

    added more absclock string elements

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # absclock
 The Absolute Clock
+
+Represents a single unique moment since the dawn of time as a colon-delimted string. Example:
+
+`E:2025:07:23:09:28:15`
+
+The Format:
+
+`E`: Eternity. The scope of all time. This is an invariant `E`.
+`O`: Eon. Can be Hadean `H`, Archean `A`, Proterozoic `R`, or Phanerozoic `P` (the current eon).
+`YYYY`: The current year. Unbounded.
+`MM`: The current month. 01-12.
+`DD`: The current day. 01-31.
+`HH`: The current hour. Military format. 00-23.
+`MI`: The current minute. 00-59.
+`SS`: The current second. 00-59.
+
+Time is represented in Zulu time.
--- absclock.go
+++ absclock.go
@@ -2,16 +2,56 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
+	// Get the current time broken into its components.
+	// Time is represented in Zulu time.
+	now := time.Now().UTC()
+	year := strconv.Itoa(now.Year())
+	month := strconv.Itoa(int(now.Month()))
+	day := strconv.Itoa(now.Day())
+	hour := strconv.Itoa(now.Hour())
+	minute := strconv.Itoa(now.Minute())
+	second := strconv.Itoa(now.Second())
+
+	// Pad the values with a leading zero if tanyhey are less than 10.
+	if now.Month() < 10 {
+		month = "0" + month
+	}
+	if now.Day() < 10 {
+		day = "0" + day
+	}
+	if now.Hour() < 10 {
+		hour = "0" + hour
+	}
+	if now.Minute() < 10 {
+		minute = "0" + minute
+	}
+	if now.Second() < 10 {
+		second = "0" + second
+	}
+
+	// Build the string
 	var builder strings.Builder
-	builder.WriteString("Current time: ")
-	builder.WriteString(time.Now().String())
+	builder.WriteString("E:")
+	builder.WriteString("P:")
+	builder.WriteString(year)
+	builder.WriteString(":")
+	builder.WriteString(month)
+	builder.WriteString(":")
+	builder.WriteString(day)
+	builder.WriteString(":")
+	builder.WriteString(hour)
+	builder.WriteString(":")
+	builder.WriteString(minute)
+	builder.WriteString(":")
+	builder.WriteString(second)
 
-	result := builder.String()
+	absTime := builder.String()
 
-	fmt.Println(result)
+	fmt.Println(absTime)
 }
