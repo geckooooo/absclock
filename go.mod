dc700e7d382fbe40236e56c200ed243abe3914af
Jason Wells <jason@wells.me>
2025-07-23 09:13:29 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module absclock
+
+go 1.22.6
---
+++ old/AbsoluteClockJava/AbsoluteClock/src/jason/absclock/AbsoluteClock.java
@@ -0,0 +1,26 @@
+package jason.absclock;
+
+public class AbsoluteClock {
+	private AbsoluteTime time;
+	
+	public AbsoluteTime getTime() {
+		return time;
+	}
+
+	public void setTime(AbsoluteTime time) {
+		this.time = time;
+	}
+
+	public static void main(String[] args) {
+		new AbsoluteClock();
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 */
+	public AbsoluteClock() {
+		time = new AbsoluteTime();
+		System.out.println(time);
+	}
+}
